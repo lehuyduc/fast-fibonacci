1284f62791a5b815df49d4a99fd8b7ff062180fe
lehuyduc <lehuyduc3@gmail.com>
2024-12-26 11:08:54 +0700

    Fix full_run.sh

--- full_run.sh
+++ full_run.sh
@@ -1,7 +1,7 @@
 curl -O https://gmplib.org/download/gmp/gmp-6.3.0.tar.xz
 tar -xf gmp-6.3.0.tar.xz
 mv gmp-6.3.0/* .
-./configure enable-cxx
+./configure --enable-cxx
 make -j16
 export LD_LIBRARY_PATH="./.libs:$LD_LIBRARY_PATH"
 g++ -o exe -I./ -L./.libs main.cpp -O3 -march=native -lgmpxx -lgmp
